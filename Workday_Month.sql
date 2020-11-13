2b4b73d0887ebdb655dbf13760ad3a3c367e7aee
Felipe <60966475+fevieira27@users.noreply.github.com>
2020-11-13 17:03:19 +0000

    Changing EXTRACT(DAY FROM calendar_date) by day_of_month

--- Workday_Month.sql
+++ Workday_Month.sql
@@ -1,6 +1,6 @@
 SELECT calendar_date
       ,day_of_month
      	- week_of_month * 2
-     	+ (CASE WHEN td_day_of_week(calendar_date - EXTRACT(DAY FROM calendar_date) + 1) = 1	THEN 1 ELSE 0 END)
+     	+ (CASE WHEN td_day_of_week(calendar_date - day_of_month + 1) = 1	THEN 1 ELSE 0 END)
      	- (CASE WHEN day_of_week=7 THEN 1 ELSE 0 END) as WorkdayNumber
 FROM Sys_Calendar.Calendar	
