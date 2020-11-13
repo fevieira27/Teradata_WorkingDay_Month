5764c7b4207315e1161cdbca9ce097c100bc0ed6
Felipe <60966475+fevieira27@users.noreply.github.com>
2020-11-13 17:00:49 +0000

    Update Workday_Month.sql

--- Workday_Month.sql
+++ Workday_Month.sql
@@ -2,4 +2,5 @@ SELECT calendar_date
       ,day_of_month
      	- week_of_month * 2
      	+ (CASE WHEN td_day_of_week(calendar_date - EXTRACT(DAY FROM calendar_date) + 1) = 1	THEN 1 ELSE 0 END)
-     	- (CASE WHEN day_of_week=7 THEN 1 ELSE 0 END) as WorkdayNumber     FROM Sys_Calendar.Calendar	
+     	- (CASE WHEN day_of_week=7 THEN 1 ELSE 0 END) as WorkdayNumber
+FROM Sys_Calendar.Calendar	
