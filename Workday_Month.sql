ac99620bf59cf75f00fc61d1789614c59ac87e8c
Felipe <60966475+fevieira27@users.noreply.github.com>
2024-01-03 21:51:02 +0000

    Update Workday_Month.sql

--- Workday_Month.sql
+++ Workday_Month.sql
@@ -1,7 +1,9 @@
+-- SQL code that calculates the working day of the month based on any date used as a parameter (calendar_date). This does not account for holidays, though, as they are country-specific.
+
 SELECT calendar_date
-      ,day_of_month
-     	- week_of_month * 2
-     	+ (CASE WHEN td_day_of_week(calendar_date - day_of_month + 1) = 1 THEN 1 ELSE 0 END)
-     	- (CASE WHEN day_of_week=7 THEN 1 ELSE 0 END) as WorkdayNumber
+     , day_of_month
+     	  - week_of_month * 2
+        + (CASE WHEN td_day_of_week(calendar_date - day_of_month + 1) = 1 THEN 1 ELSE 0 END)
+     	  - (CASE WHEN day_of_week=7 THEN 1 ELSE 0 END) as WorkdayNumber
 FROM Sys_Calendar.Calendar
 ;
